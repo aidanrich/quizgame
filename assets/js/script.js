b340cffc1272b3eb411df70b9791d623566e1f65
aorich <rich.aidan@gmail.com>
2021-09-11 13:40:33 -0400

    making more variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,16 @@ var startGame = document.querySelector(".question");
 var startButton = document.querySelector(".startbutton");
 var startButton2 = document.createElement("input");
 
+var answer2 = document.querySelector(".answerbutton2");
+var button2 = document.createElement("input");
+
+var answer3 = document.querySelector(".answerbutton3");
+var button3 = document.createElement("input");
+
+var answer4 = document.querySelector(".answerbutton4");
+var button4 = document.createElement("input");
+
+
 function quizGame() {
     // Need a starting page with start button
 
@@ -23,7 +33,7 @@ function quizGame() {
     startButton.appendChild(startButton2)
     startButton2.setAttribute("type", "button")
     startButton2.setAttribute("value", "Start Quiz!")
-    
+
     // add event listener to start game
     startButton.addEventListener("click", function () {
         // send message to start game?
@@ -33,6 +43,9 @@ function quizGame() {
 
     function sendMessage() {
         startGame.textContent = "Quiz started!"
+        startButton.appendChild(startButton2)
+        startButton2.setAttribute("type", "button")
+        startButton2.setAttribute("value", "Go Go!")
     }
 
     // quize pages need timer
